7c55c92d418cea88817da87fb78bdcb180ad4a52
stefcosmin09-glitch <stef.cosmin09@gmail.com>
2026-07-10 22:28:35 +0000

    added pytest tests

---
+++ test_app.py
@@ -0,0 +1,11 @@
+from app import app
+
+def test_home_page():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_about_page():
+    client = app.test_client()
+    response = client.get('/about')
+    assert response.status_code == 200
